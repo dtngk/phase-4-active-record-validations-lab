df915d5bbdbf7c4eb02b4e98b9254dc912dd7dc8
Dat Nguyen <dnguyenk2@gmail.com>
2022-08-21 10:22:30 -0700

    Completed Assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validate :clickbaity
+
+    CLICK_BAIT_REGEX_PATTERNS = [/Won't Believe/, /Secret/, /Top \d/, /Guess/]
+
+    def clickbaity 
+        if CLICK_BAIT_REGEX_PATTERNS.none? { |p| p.match title}
+            errors.add(:title, "ClickBait Title")
+        end
+    end
 end
